2d75eb3a97ad2ecc8d0001209d9bef6e5f392339
Emanuel-Gmz <gomezema457@gmail.com>
2026-05-07 21:12:09 -0300

    feat (TSDS-11): Creacion de Clases sobre Partido
    Se crean las clases sobre Partidos entidad,Dao

---
+++ src/main/java/entities/Partido.java
@@ -0,0 +1,76 @@
+package entities;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Partido {
+    private int idPartido;
+    private String equipolocal;
+    private String equipoVisitante;
+    //private Etapa etapa;
+    //private Estadio estadio;
+    private Date fechaHora;
+    private int golesLocal;
+    private int golesVisitante;
+    private boolean finalizado;
+
+
+    //Se crea el constructor y se lo deja comentado debido a que no dispongo de las demas entidades.
+    /*public Partido(int idPartido, String equipolocal, String equipoVisitante, Etapa etapa, Estadio estadio, Date fechaHora, int golesLocal, int golesVisitante, boolean finalizado) {
+        this.idPartido = idPartido;
+        this.equipolocal = equipolocal;
+        this.equipoVisitante = equipoVisitante;
+        this.etapa = etapa;
+        this.estadio = estadio;
+        this.fechaHora = fechaHora;
+        this.golesLocal = golesLocal;
+        this.golesVisitante = golesVisitante;
+        this.finalizado = finalizado;
+    }
+     */
+
+    //GET
+    public int getIdPartido() {return idPartido;}
+    public String getEquipolocal() {return equipolocal;}
+    public String getEquipoVisitante() {return equipoVisitante;}
+    public Date getFechaHora() {return fechaHora;}
+    public int getGolesLocal() {return golesLocal;}
+    public int getGolesVisitante() {return golesVisitante;}
+    public boolean isFinalizado() {return finalizado;}
+
+    //SETT
+    public void setIdPartido(int idPartido) {this.idPartido = idPartido;}
+    public void setEquipolocal(String equipolocal) {this.equipolocal = equipolocal;}
+    public void setEquipoVisitante(String equipoVisitante) {this.equipoVisitante = equipoVisitante;}
+    public void setFechaHora(Date fechaHora) {this.fechaHora = fechaHora;}
+    public void setGolesLocal(int golesLocal) {this.golesLocal = golesLocal;}
+    public void setGolesVisitante(int golesVisitante) {this.golesVisitante = golesVisitante;}
+    public void setFinalizado(boolean finalizado) {this.finalizado = finalizado;}
+
+    @Override
+    public String toString() {
+        return "Partido{" +
+                "idPartido=" + idPartido +
+                ", equipolocal='" + equipolocal + '\'' +
+                ", equipoVisitante='" + equipoVisitante + '\'' +
+                ", fechaHora=" + fechaHora +
+                ", golesLocal=" + golesLocal +
+                ", golesVisitante=" + golesVisitante +
+                ", finalizado=" + finalizado +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Partido partido = (Partido) o;
+        return idPartido == partido.idPartido && golesLocal == partido.golesLocal && golesVisitante == partido.golesVisitante && Objects.equals(equipolocal, partido.equipolocal) && Objects.equals(equipoVisitante, partido.equipoVisitante) && Objects.equals(fechaHora, partido.fechaHora) && finalizado == partido.finalizado;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idPartido, equipolocal, equipoVisitante, fechaHora, golesLocal, golesVisitante, finalizado);
+    }
+
+
+}
